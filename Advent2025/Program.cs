2797943297531dcc3658df5705bb4514313aa920
Eduardo Quezada <eduardo602002@gmail.com>
2025-12-03 12:06:40 -0300

    I misread the question :(

--- Advent2025/Program.cs
+++ Advent2025/Program.cs
@@ -1,11 +1,9 @@
-﻿namespace Advent2025
+﻿using System.Text.RegularExpressions;
+
+namespace Advent2025
 {
     internal class Program
     {
-        enum Modo
-        {
-            MODO_MENU_PRINCIPAL,
-        };
         static void Main(string[] args)
         {
             bool running = true;
@@ -14,8 +12,10 @@
             {
                 Console.Clear();
                 Console.WriteLine("Elija una opción:");
-                Console.WriteLine("1.- 1 de Diciembre (Parte 1)");
-                Console.WriteLine("2.- 1 de Diciembre (Parte 2)");
+                Console.WriteLine("1.- Dec 1, Secret Entrance (Part 1)");
+                Console.WriteLine("2.- Dec 1, Secret Entrance (Part 2)");
+                Console.WriteLine("3.- Dec 2, Gift Shop (Part 1)");
+                Console.WriteLine("4.- Dec 2, Gift Shop (Part 2)");
                 bool seleccionado = false;
 
                 while (!seleccionado) {
@@ -30,6 +30,8 @@
                     {
                         case "1": Diciembre_1(false); break;
                         case "2": Diciembre_1(true); break;
+                        case "3": Diciembre_2(false); break;
+                        case "4": Diciembre_2(true); break;
                         default:
                             Console.WriteLine("Input no válido");
                             seleccionado = false;
@@ -37,6 +39,77 @@
                     }
                 }
             }
+        }
+
+        static void Diciembre_2(bool parte2)
+        {
+            Console.Clear();
+            if (parte2)
+                Console.WriteLine("Diciembre 2 (Parte 2): Presione Enter para iniciar el proceso.");
+            else
+                Console.WriteLine("Diciembre 2 (Parte 1): Presione Enter para iniciar el proceso.");
+            Console.ReadLine();
+
+            ulong sumaIDInvalidos = 0;
+
+            foreach (string line in File.ReadLines("res/dec2input.txt"))
+            {
+                string[] rangos = line.Split(",");
+                for (int i = 0; i < rangos.Length; i++)
+                {
+                    string[] rango = rangos[i].Split("-");
+                    ulong inicio = ulong.Parse(rango[0]);
+                    ulong fin = ulong.Parse(rango[1]);
+
+                    for (ulong j = inicio; j <= fin; j++)
+                    {
+                        string texto = j.ToString();
+                        int largo = texto.Length;
+
+                        //Console.WriteLine($"{j}");
+                        bool esInvalido = false;
+                        for (int k = 1; k < largo && !esInvalido; k++)
+                        {
+                            if (largo % k != 0)
+                                continue;
+                            //Console.WriteLine($"  k:{k}");
+                            string inicial = texto.Substring(0, k);
+                            bool patronEsIgual = true;
+                            for (int l = 0; l < largo / k; l++)
+                            {
+                                string subPatron = texto.Substring(l * k, k);
+                                if (!inicial.Equals(subPatron))
+                                    patronEsIgual = false;
+                                //Console.WriteLine($"     l:{l}");
+                                //Console.WriteLine($"    l:{l}, inicial:{inicial}, substr:{subPatron},");
+                            }
+                            if (patronEsIgual)
+                            {
+                                //Console.WriteLine("PATRON IGUAL");
+                                esInvalido = true;
+                                break;
+                            }
+                            //if ()
+                        }
+                        if (esInvalido)
+                        {
+                            sumaIDInvalidos += j;
+                            Console.WriteLine($"{j}");
+                        }
+
+
+                        //if (Regex.Count(j.ToString(), @"(\d+)(\1{1,})") > 0)
+                        //{
+                        //    sumaIDInvalidos += j;
+                        //    Console.WriteLine($"{j}");
+                        //}
+                    }
+                    //Console.WriteLine(rangos[i]);
+                }
+            }
+            Console.WriteLine($"\nFin: {sumaIDInvalidos}");
+
+            Console.ReadLine();
         }
         static void Diciembre_1 (bool considerarRotaciones)
         {
